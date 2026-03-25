ace759d096795687667b3bf0ec5a514fbe3774d4
SkillSageDev <147807207+SkillSageDev@users.noreply.github.com>
2026-03-25 21:38:50 +0200

    fix: sops-nix can't decrypt

--- modules/git.nix
+++ modules/git.nix
@@ -1,13 +1,12 @@
-{ config, osConfig, ... }:
+{ osConfig, ... }:
 
 {
   programs.git = {
     enable = true;
     settings = {
-      # FIXME: known bug, nixos fails to decrypt the secrets when rebuilding.
       user = {
-        name = osConfig.sops.secrets.git_username.path;
-	email = osConfig.sops.secrets.git_email.path;
+        name = "cat ${osConfig.sops.secrets.git_username.path}";
+	email = "cat ${osConfig.sops.secrets.git_email.path}";
       };
 
       init.defaultBranch = "main";
